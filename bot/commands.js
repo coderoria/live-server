ba7d91780675b839dc761181af1153efdd14f42c
Noah Groß <gross@ngsger.de>
2021-06-11 09:43:27 +0200

    Add more tests for commands

--- bot/commands.js
+++ bot/commands.js
@@ -152,7 +152,7 @@ function executeCredit(bot, matches, userstate) {
 }
 
 function executeDonation(bot, matches, userstate) {
-    findRecipient(matches, userstate);
+    let recipient = findRecipient(matches, userstate);
     let donation = "@" + recipient + ", hier ist unser Donationlink: "; //LINK MISSING
     bot.say(channel, donation);
 }
@@ -179,10 +179,9 @@ function executeAccountAge(bot, matches, userstate) {
 }
 
 function executeUptime(bot, matches, userstate) {
-    findRecipient(matches, userstate);
+    let recipient = findRecipient(matches, userstate);
     //UPTIME MISSING
     let upTime = " ";
-    ("");
     let message = "@" + recipient + ", CodeRoria ist seit " + upTime + " live.";
     bot.say(channel, message);
 }
@@ -214,9 +213,7 @@ function executeQuotes(bot, matches, userstate) {
     let requiredLevel = "moderator";
     let newQuote = "";
     if (matches.shift() === "add" && hasPermission(userstate, requiredLevel)) {
-        for (let i in matches) {
-            newQuote += matches[i] + " ";
-        }
+        newQuote = matches.join(" ");
         pool.query(
             "INSERT INTO quotes (quote) VALUES (?);",
             newQuote,
@@ -241,7 +238,7 @@ function executeQuotes(bot, matches, userstate) {
                 bot.say(channel, "Es gibt derzeit keine Zitate :(");
                 return;
             }
-            bot.say(channel, res[0].quote);
+            bot.say(channel, dbres[0].quote);
         }
     );
 }
@@ -276,7 +273,7 @@ function executeCounters(bot, matches, userstate) {
         }
         pool.query(
             "INSERT INTO counter (name) VALUES (?);",
-            matches[1],
+            matches[1].toLowerCase(),
             (error) => {
                 if (error) {
                     if (error.code === "ER_DUP_KEY") {
@@ -322,18 +319,18 @@ function executeCounters(bot, matches, userstate) {
                     return;
                 }
                 let points = result[0].count;
-                const re = /([+-])(\d)*/;
+                const re = /([+-])(\d*)/;
                 let groups = re.exec(matches[1]);
                 if (groups[1] == "+") {
-                    if (groups.length == 3) {
-                        points += groups[2];
+                    if (groups[2].length > 0) {
+                        points += parseInt(groups[2]);
                     } else {
                         points += 1;
                     }
                 }
                 if (groups[1] == "-") {
-                    if (groups.length == 3) {
-                        points -= groups[2];
+                    if (groups[2].length > 0) {
+                        points -= parseInt(groups[2]);
                     } else {
                         points -= 1;
                     }
--- test/commandsTest.js
+++ test/commandsTest.js
@@ -17,7 +17,7 @@ beforeEach(function () {
 });
 
 afterEach(function () {
-    botMock.verify();
+    sinon.restore();
 });
 
 describe("Commands", function () {
@@ -31,6 +31,7 @@ describe("Commands", function () {
             .once()
             .alwaysCalledWith(undefined, sinon.match(/^@TestUser.*/));
         commands.checkCommand(bot, "!discord @TestUser", userstate);
+        botMock.verify();
     });
 
     describe("Permission checks (user has mod+sub)", function () {
@@ -55,57 +56,238 @@ describe("Commands", function () {
         it("Discord command", function () {
             botMock.expects("say").once();
             commands.checkCommand(bot, "!discord", userstate);
+            botMock.verify();
         });
 
         it("Twitter command", function () {
             botMock.expects("say").once();
             commands.checkCommand(bot, "!twitter", userstate);
+            botMock.verify();
         });
 
         it("Github command", function () {
             botMock.expects("say").once();
             commands.checkCommand(bot, "!git", userstate);
+            botMock.verify();
         });
 
         it("Credit command", function () {
             botMock.expects("say").once();
             commands.checkCommand(bot, "!credit", userstate);
+            botMock.verify();
         });
 
         it("Watchtime command", function () {
             botMock.expects("say").once();
             commands.checkCommand(bot, "!watchtime", userstate);
+            botMock.verify();
         });
 
         it("Followage command", function () {
             botMock.expects("say").once();
             commands.checkCommand(bot, "!followage", userstate);
+            botMock.verify();
         });
 
         it("Accountage command", function () {
             botMock.expects("say").once();
             commands.checkCommand(bot, "!accountage", userstate);
+            botMock.verify();
         });
 
         it("Commands command", function () {
             botMock.expects("say").once();
             commands.checkCommand(bot, "!commands", userstate);
+            botMock.verify();
         });
 
-        it("Shoutout command", function () {
-            botMock
-                .expects("say")
-                .once()
-                .neverCalledWith(undefined, "kein channel angegeben.");
-            commands.checkCommand(bot, "!shoutout testuser", userstate);
+        describe("Shoutout command", function () {
+            it("!shoutout testuser", function () {
+                botMock
+                    .expects("say")
+                    .once()
+                    .neverCalledWith(undefined, "kein channel angegeben.");
+                commands.checkCommand(bot, "!shoutout testuser", userstate);
+                botMock.verify();
+            });
+
+            it("!shoutout", function () {
+                botMock
+                    .expects("say")
+                    .once()
+                    .alwaysCalledWith(undefined, "kein channel angegeben.");
+                commands.checkCommand(bot, "!shoutout", userstate);
+                botMock.verify();
+            });
         });
 
-        it("Shoutout command (invalid)", function () {
-            botMock
-                .expects("say")
-                .once()
-                .alwaysCalledWith(undefined, "kein channel angegeben.");
-            commands.checkCommand(bot, "!shoutout", userstate);
+        it("Donation command", function () {
+            botMock.expects("say").once();
+            commands.checkCommand(bot, "!donation", userstate);
+            botMock.verify();
+        });
+
+        it("Uptime command", function () {
+            botMock.expects("say").once();
+            commands.checkCommand(bot, "!uptime", userstate);
+            botMock.verify();
+        });
+
+        it("Lurk command", function () {
+            botMock.expects("say").once();
+            commands.checkCommand(bot, "!lurk", userstate);
+            botMock.verify();
+        });
+
+        describe("Quote command", function () {
+            let pool = require("../server/database");
+
+            it("Random quote", function () {
+                let stub = sinon
+                    .stub(pool, "query")
+                    .yields(null, [{ quote: "Test" }]);
+                botMock.expects("say").once().calledWith(undefined, "Test");
+                commands.checkCommand(bot, "!quote", userstate);
+                botMock.verify();
+            });
+
+            it("No quote available", function () {
+                let stub = sinon.stub(pool, "query").yields(null, []);
+                botMock.expects("say").once();
+                commands.checkCommand(bot, "!quote", userstate);
+                botMock.verify();
+            });
+
+            it("Add new quote", function () {
+                let stub = sinon.stub(pool, "query").yields(null);
+                botMock.expects("say").once();
+                commands.checkCommand(bot, "!quote add hehe lol", userstate);
+                assert.strictEqual(stub.lastCall.args[1], "hehe lol");
+                botMock.verify();
+            });
+        });
+
+        describe("Counter command", function () {
+            let pool = require("../server/database");
+            let stub;
+
+            it("Get counter", function () {
+                let stub = sinon
+                    .stub(pool, "query")
+                    .yields(null, [{ name: "testcounter", count: 1000 }]);
+                botMock.expects("say").once();
+                commands.checkCommand(bot, "!counter testcounter", userstate);
+                botMock.verify();
+            });
+
+            it("Get counter as regular user", function () {
+                let userstateCounter = { badges: {} };
+                let stub = sinon
+                    .stub(pool, "query")
+                    .yields(null, [{ name: "testcounter", count: 1000 }]);
+                botMock.expects("say").once();
+                commands.checkCommand(
+                    bot,
+                    "!counter testcounter",
+                    userstateCounter
+                );
+                botMock.verify();
+            });
+
+            it("Counter not found", function () {
+                let stub = sinon.stub(pool, "query").yields(null, []);
+                botMock.expects("say").once();
+                commands.checkCommand(bot, "!counter testcounter", userstate);
+                botMock.verify();
+            });
+
+            it("Add 1 (+) to counter", function () {
+                let stub = sinon
+                    .stub(pool, "query")
+                    .onCall(0)
+                    .yields(null, [{ name: "testcounter", count: 1000 }])
+                    .onCall(1)
+                    .yields(null);
+                botMock.expects("say").once();
+                commands.checkCommand(bot, "!counter testcounter +", userstate);
+                assert.strictEqual(stub.lastCall.args[1][0], 1001);
+                botMock.verify();
+            });
+
+            it("Subtract 1 (-) from counter", function () {
+                let stub = sinon
+                    .stub(pool, "query")
+                    .onCall(0)
+                    .yields(null, [{ name: "testcounter", count: 1000 }])
+                    .onCall(1)
+                    .yields(null);
+                botMock.expects("say").once();
+                commands.checkCommand(bot, "!counter testcounter -", userstate);
+                assert.strictEqual(stub.lastCall.args[1][0], 999);
+                botMock.verify();
+            });
+
+            it("Add 25 (+25) to counter", function () {
+                let stub = sinon
+                    .stub(pool, "query")
+                    .onCall(0)
+                    .yields(null, [{ name: "testcounter", count: 1000 }])
+                    .onCall(1)
+                    .yields(null);
+                botMock.expects("say").once();
+                commands.checkCommand(
+                    bot,
+                    "!counter testcounter +25",
+                    userstate
+                );
+                assert.strictEqual(stub.lastCall.args[1][0], 1025);
+                botMock.verify();
+            });
+
+            it("Subtract 25 (-25) from counter", function () {
+                let stub = sinon
+                    .stub(pool, "query")
+                    .onCall(0)
+                    .yields(null, [{ name: "testcounter", count: 1000 }])
+                    .onCall(1)
+                    .yields(null);
+                botMock.expects("say").once();
+                commands.checkCommand(
+                    bot,
+                    "!counter testcounter -25",
+                    userstate
+                );
+                assert.strictEqual(stub.lastCall.args[1][0], 975);
+                botMock.verify();
+            });
+
+            it("Add new counter", function () {
+                let stub = sinon.stub(pool, "query").yields(null);
+                botMock
+                    .expects("say")
+                    .once()
+                    .calledWith(undefined, sinon.match(/.*testcounter.*/));
+                commands.checkCommand(
+                    bot,
+                    "!counter add testcounter",
+                    userstate
+                );
+                assert.strictEqual(stub.lastCall.args[1], "testcounter");
+                botMock.verify();
+            });
+
+            it("Add duplicate counter", function () {
+                let stub = sinon
+                    .stub(pool, "query")
+                    .yields({ code: "ER_DUP_KEY" });
+                botMock.expects("say").once();
+                commands.checkCommand(
+                    bot,
+                    "!counter add testcounter",
+                    userstate
+                );
+                botMock.verify();
+            });
         });
     });
 });
