d1faa996051ce378b1c61de43e0f2f82a9649e69
Noah Groß <gross@ngsger.de>
2021-10-01 21:28:46 +0200

    Inherit general overlay script

--- static/js/overlay/overlay.js
+++ static/js/overlay/overlay.js
@@ -1,39 +1,8 @@
-const socket = io();
-
-socket.on("channel.follow", (event) => {
-    let alertBox = document.querySelector(".alert-box");
-    if (alertBox == undefined) return;
-    window[alertBox.dataset.function](
-        chooseSanitized(event.user_name, event.user_login),
-        "follow"
-    );
-});
-
-socket.on("channel.subscribe", (event) => {
-    let alertBox = document.querySelector(".alert-box");
-    if (alertBox == undefined) return;
-    window[alertBox.dataset.function](
-        chooseSanitized(event.user_name, event.user_login),
-        "sub"
-    );
-});
-
-socket.on("channel.subscription.message", (event) => {
-    let alertBox = document.querySelector(".alert-box");
-    if (alertBox == undefined) return;
-    window[alertBox.dataset.function](
-        chooseSanitized(event.user_name, event.user_login),
-        "sub",
-        event.message
-    );
-});
-
-socket.on("playback", (img, artists, title) => {
-    let playBackBox = document.querySelector(".playback-box");
-    if (playBackBox == undefined) return;
-    window[playBackBox.dataset.function](img, artists, title);
-});
-
-function chooseSanitized(displayName, userName) {
-    return displayName.toLowerCase() === userName ? displayName : userName;
-}
+window.onerror = (event) => {
+    socket.emit("error", {
+        error: event,
+        url: window.location.href,
+        agent: navigator.userAgent,
+        socket: socket.id,
+    });
+};
