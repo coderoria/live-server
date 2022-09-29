69b7f1d9b6df1635ec7f028f9dd645c7c0cc2681
Noah Groß <me@codesalat.dev>
2022-09-29 18:51:07 +0200

    Make deploy ready

--- server/database.ts
+++ server/database.ts
@@ -1,10 +1,2 @@
 import * as mysql from "mysql";
-export var pool: mysql.Pool = mysql.createPool({
-  connectionLimit: 10,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT as number | undefined,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  timeout: 60000,
-});
+export var pool: mysql.Pool = mysql.createPool(process.env.DATABASE_URL);
